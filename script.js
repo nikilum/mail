95e8d73312781b4e4dc08ecf727a64e4f9743b89
lumpov2004 <lumpov2004@mail.ru>
2020-12-02 00:10:37 +0500

    Completed popup

--- script.js
+++ script.js
@@ -2,8 +2,28 @@
 
 function showEmailSendWindow(){
     Swal.fire({
-
-
-
+        width:700,
+        html:
+            '<button class="popup-button">B</button>' +
+            '<button class="popup-button">I</button>' +
+            '<button class="popup-button">U</button>' +
+            '<br>' +
+            '<textarea style="width: 600px; height: 400px; resize: none"></textarea>' +
+            '<style>' +
+            '.popup-button {' +
+            'margin:5px; ' +
+            'width: 35px;' +
+            'height:35px;' +
+            'border: none;' +
+            '}' +
+            '.popup-button:hover {' +
+            'background: white;' +
+            '}' +
+            '.popup-button:focus {' +
+            'outline: none;' +
+            '}' +
+            '' +
+            '</style>',
+        confirmButtonText: 'Send',
     })
 }
